7699975881ace501786c604eb1832330842b2fa2
Oleg Romanov <sqdcontrast@gmail.com>
2025-07-26 00:45:36 +0300

    feat(routes): change anime.form and anime.search routes to '/' endpoint

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\AnimeSearchController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/search', [AnimeSearchController::class, 'showForm'])->name('anime.form');
-Route::post('/search', [AnimeSearchController::class, 'search'])->name('anime.search');
+Route::get('/', [AnimeSearchController::class, 'showForm'])->name('anime.form');
+Route::post('/', [AnimeSearchController::class, 'search'])->name('anime.search');
