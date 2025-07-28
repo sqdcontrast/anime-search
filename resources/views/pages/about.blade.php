417882920790bf2ac1f0ef7e6ca0f38911004964
Oleg Romanov <sqdcontrast@gmail.com>
2025-07-28 13:05:34 +0300

    feat(about): add about view and route

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title')
+    о проекте
+@endsection
+
+@section('content')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AnimeSearchController::class, 'showForm'])->name('anime.form');
 Route::post('/', [AnimeSearchController::class, 'search'])->name('anime.search');
+
+Route::get('/about', function () {
+  return view('pages.about');
+})->name('pages.about');
