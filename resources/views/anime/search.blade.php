32638a5cb20cac7d47683106a587c2461aec1081
Oleg Romanov <sqdcontrast@gmail.com>
2025-07-30 17:20:30 +0300

    feat(anime-search): add mb classes to elements; add heading component

--- resources/views/anime/search.blade.php
+++ resources/views/anime/search.blade.php
@@ -1,33 +1,36 @@
 @extends('layouts.app')
 
 @section('title')
-    {{ __('поиск') }}
+  {{ __('поиск') }}
 @endsection
 
 @section('content')
-    <div class="container px-4 mx-auto">
+  <div class="container px-4 mx-auto">
 
-        <section class="text-white py-20">
+    <section class="text-white py-20">
 
-            <div class="text-center">
-                <h1 class="uppercase text-6xl font-bold mb-3 text-shadow-[0_0_8px_rgba(255,255,255,0.5)]">
-                    {{ __('Anime search') }}
-                </h1>
-            </div>
+      <div class="text-center mb-20">
+        {{-- Heading --}}
+        <x-heading>
+          {{ __('Anime search') }}
+        </x-heading>
+      </div>
 
-            <div>
+      <div>
 
-                <ul>
-                    @foreach ($animeResults as $anime)
-                        <li class="py-10 border-b-4 border-neutral-700">
-                            <x-anime.anime-card :anime="$anime" />
-                        </li>
-                    @endforeach
-                </ul>
+        {{-- Anime list --}}
+        <ul>
+          @foreach ($animeResults as $anime)
+            <li class="py-10 border-b-4 border-neutral-700">
+              {{-- Anime card --}}
+              <x-anime.anime-card :anime="$anime" />
+            </li>
+          @endforeach
+        </ul>
 
-            </div>
+      </div>
 
-        </section>
+    </section>
 
-    </div>
+  </div>
 @endsection
