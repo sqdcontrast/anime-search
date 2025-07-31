e2fa5a5618db77a2b668b8acf9d8bb8f8ea73dc1
Oleg Romanov <sqdcontrast@gmail.com>
2025-07-31 13:44:04 +0300

    feat(footer): add footer markup

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,5 @@
-<footer></footer>
+<footer class="p-4 text-white text-sm text-center w-full">
+  © {{ date('Y') }}
+  <a href="https://github.com/sqdcontrast" target="_blank" class="underline hover:text-">{{ __('sqdcontrast.') }}</a>
+  {{ __('All rights reserved.') }} 💙
+</footer>
